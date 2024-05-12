b0e7b2274c80297b41a089ab44769abee7f4f607
JokerBrando <23a_mvp@liceum.ztu.edu.ua>
2024-05-12 21:45:33 +0300

    написав дофіга коду і закоментував

--- market.js
+++ market.js
@@ -20,3 +20,79 @@ for(let i = 0; i< 100; i++){
 }else {
     console.log('Блок товарів не знайдено')
 }
+
+
+// let itemsArray = [
+// 'Газонокосарка 43',
+// 'Електричний тример 110',
+// 'Електрична газонокосарка 32',
+// 'Акумуляторний оприскувач 12 N',
+// 'Газонокосарка 430',
+// 'Електричний тример 130',
+// 'Електрична газонокосарка 320',
+// 'Акумуляторний оприскувач 12 E',
+// ]
+
+// itemsArray = itemsArray.sort()
+
+// for (let i=0; i < itemsArray.length; i++){
+//     console.log(i + '-й елемент: ', itemsArray[i])
+// }
+
+// itemsArray.sort().forEach((item) => {
+//     console.log(item + '-й елемент: ', item)
+// })
+
+
+// let itemsArray = [
+//     'Газонокосарка 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N',
+//     ]
+
+// itemsArray.forEach((item) =>{
+//     console.log(item)
+// })
+
+
+// if (itemsDiv){
+//     itemsArray.forEach((item) =>{
+//         console.log(item)
+//     })
+// } else {
+//     console.log('Блок товарів не знайдено')
+// }
+
+// let itemsArray = [
+//     'Газонокосарка 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N',
+// ]
+
+// let itemsDiv = document.getElementById("items");
+
+// if (itemsDiv) {
+//     itemsArray.forEach((item) => {
+//         console.log(item)
+//     })
+// } else {
+//     console.log('Блок товарів не знайдено')
+// }
+
+
+
+// if (itemsDiv) {
+//     itemsArray.forEach((item) => {
+//         itemsDiv.innerHTML += 
+//         `
+//         <div class = "item">
+//         <h2>Товар №${index}</h2>
+//         <p>${item}</p>
+//         </div>
+//         `
+//     })
+// } else {
+//     console.log('Блок товарів не знайдено')
+// }
