44b24b745856c995b9b174c01c37b837f1c0fda8
JokerBrando <23a_mvp@liceum.ztu.edu.ua>
2024-05-15 09:11:33 +0300

    був закоментований тоді не докінця правильний код

--- market.js
+++ market.js
@@ -1,47 +1,89 @@
 
 console.log('Перевірка підключеного файлу скриптів market.js')
 
+//визначення масиву товарів
+let itemsArray = ['Газонокосарка 56',
+'Електриччний триметр',
+'Електрична газонокосарка',
+'Акумулятор',
+'Електриччний триметр',
+'Електрична газонокосарка',
+'Акумулятор',
+'Електриччний триметр',
+'Електрична газонокосарка',
+'Акумулятор',
+'Електриччний триметр',
+'Електрична газонокосарка',
+'Акумулятор',
+'Електриччний триметр',
+'Електрична газонокосарка',
+'Акумулятор',
+'Електриччний триметр',
+'Електрична газонокосарка',
+'Акумулятор',
+'Електриччний триметр',
+'Електрична газонокосарка',
+'Акумулятор',
+]
 
 let itemsDiv = document.getElementById("items");
 
 if (itemsDiv){
-    console.log(itemsDiv)
-    // console.log('Поле classList: ', itemsDiv.classList)
-    // console.log('Поле id: ', itemsDiv.id)
-    // console.log('Поле clientWidth: ', itemsDiv.clientWidth)
-    // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+    itemsArray.forEach((item, index) =>{
+        // console.log(item)
+        itemsDiv.innerHTML += 
+        `
+        <div class = "item">
+        <h2>Товар №${index+1} з ${itemsArray.length}</h2>
+        <p>${item}</p>
+        </div>
+        `
+    })
 
-    // itemsDiv.innerText = 'Програмно доданий текст'
-    // itemsDiv.innerText += ' Другий Програмно доданий текст'
-for(let i = 0; i< 100; i++){
-    itemsDiv.innerHTML += '<div class = "item"></div>'
-}
-
-}else {
+} else {
     console.log('Блок товарів не знайдено')
 }
+//     console.log(itemsDiv)
 
+    
+//     // console.log('Поле classList: ', itemsDiv.classList)
+//     // console.log('Поле id: ', itemsDiv.id)
+//     // console.log('Поле clientWidth: ', itemsDiv.clientWidth)
+//     // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
 
-// let itemsArray = [
-// 'Газонокосарка 43',
-// 'Електричний тример 110',
-// 'Електрична газонокосарка 32',
-// 'Акумуляторний оприскувач 12 N',
-// 'Газонокосарка 430',
-// 'Електричний тример 130',
-// 'Електрична газонокосарка 320',
-// 'Акумуляторний оприскувач 12 E',
-// ]
+//     // itemsDiv.innerText = 'Програмно доданий текст'
+//     // itemsDiv.innerText += ' Другий Програмно доданий текст'
+// for(let i = 0; i< 100; i++){
+//     itemsDiv.innerHTML += '<div class = "item"></div>'
+    
+// }
+
+// }else {
+//     console.log('Блок товарів не знайдено')
+// }
+
 
 // itemsArray = itemsArray.sort()
 
-// for (let i=0; i < itemsArray.length; i++){
-//     console.log(i + '-й елемент: ', itemsArray[i])
-// }
+//  for (let i=0; i < itemsArray.length; i++){
+//      console.log(i + 1, itemsArray[i])
+//  }
 
-// itemsArray.sort().forEach((item) => {
-//     console.log(item + '-й елемент: ', item)
-// })
+//  itemsArray.sort().forEach((item) => {
+//      console.log(item + 1, item)
+//  })
+//  let itemsArray = [
+//  'Газонокосарка 43',
+//  'Електричний тример 110',
+//  'Електрична газонокосарка 32',
+//  'Акумуляторний оприскувач 12 N',
+//  'Газонокосарка 430',
+//  'Електричний тример 130',
+// 'Електрична газонокосарка 320',
+//  'Акумуляторний оприскувач 12 E',
+// ]
+
+ 
 
 
 // let itemsArray = [
